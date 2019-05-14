2019f3b8f59ad6f88f6727360502b8693eddd054
stevenzack <stevenzack@qq.com>
2019-05-14 10:15:00 +0800

    Just a Backup

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
 
+	"github.com/StevenZack/tools/strToolkit"
+	"github.com/fatih/color"
+
 	"github.com/StevenZack/tools/fileToolkit"
 )
 
 var (
-	sep  = string(os.PathSeparator)
-	conf = fileToolkit.GetHomeDir() + sep + ".gitstatus" + sep + "config.json"
+	sep     = string(os.PathSeparator)
+	conf    = fileToolkit.GetHomeDir() + sep + ".gitstatus" + sep + "config.json"
+	verbose = flag.Bool("v", false, "verbose")
 )
 
 func main() {
-	fmt.Println(gitstatusRepo("/Users/stevenzacker/go/src/github.com/StevenZack/gitstatus"))
+	flag.Parse()
+	parseRepoLine("/Users/stevenzacker/go/src/github.com/StevenZack/gitstatus")
 }
 func run() {
 	_, e := findConf()
@@ -45,8 +52,51 @@ func findConf() ([]string, error) {
 	return result, nil
 }
 
-func parseRepoLine(repoLine string) {
+func parseRepoLine(repoLine string) error {
+	if strings.HasSuffix(repoLine, "*") {
+		repos, e := readDir(repoLine[:len(repoLine)-1])
+		if e != nil {
+			return e
+		}
+		for _, repo := range repos {
+			b, e := gitstatusRepo(repo)
+			if e != nil {
+				fmt.Println("status repo:"+repo+" error :", e)
+				continue
+			}
+			handleBool(b, repo)
+		}
+		return nil
+	}
+	b, e := gitstatusRepo(repoLine)
+	if e != nil {
+		return e
+	}
+	handleBool(b, repoLine)
+	return nil
+}
+func handleBool(b bool, repo string) {
+	if b {
+		if *verbose {
+			fmt.Println(color.GreenString(repo))
+		}
+	} else {
+		fmt.Println(color.RedString(repo))
+	}
+}
 
+func readDir(dir string) ([]string, error) {
+	infos, e := ioutil.ReadDir(dir)
+	if e != nil {
+		return nil, e
+	}
+	strs := []string{}
+	for _, info := range infos {
+		if info.IsDir() {
+			strs = append(strs, strToolkit.Getrpath(dir)+info.Name())
+		}
+	}
+	return strs, nil
 }
 
 func gitstatusRepo(repo string) (bool, error) {
@@ -69,6 +119,9 @@ func gitstatusRepo(repo string) (bool, error) {
 		return false, e
 	}
 	str := string(b)
+	if strings.Contains(str, ".git") {
+		return false, errors.New(repo + " is not a git repo")
+	}
 	if strings.Contains(str, "git add") {
 		return false, nil
 	}
